2c4fe3163b60c87a608ea39e0f63de6a217dffa1
kovner chalumo <kchalumo@gmail.com>
2018-11-27 01:43:10 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,7 +1,13 @@
 # Write your code here.
 def badge_maker(name)
-  puts "Hello, my name is #{name}."
+  "Hello, my name is #{name}."
 end
 
 def batch_badge_creator(arr)
-  
+  arr.collect do |name|
+    badge_maker(name)
+  end
+  arr
+end
+
+def printer
