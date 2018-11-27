05a213bfdb73a8b1ee752f0afbf2bccfccfc2571
kovner chalumo <kchalumo@gmail.com>
2018-11-27 01:49:32 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -13,11 +13,19 @@ end
 def assign_rooms(arr)
   rarr = []
   arr.each_with_index do |val, index|
-    rarr << 
+    rarr <<  "Hello, #{val}! You'll be assigned to room #{index+1}!"
+  end
+  rarr
+end
 
 def printer(arr)
   batch_badge_creator(arr).each do |message|
     puts message 
   end
+  
+  assign_rooms.each do |assignment|
+    puts assignment
+  end
+end
   
   
